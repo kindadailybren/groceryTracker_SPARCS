7342cec590fa20145cb91dce0ede6816d3527a1f
errolJames59 <erroljamesminguez59@gmail.com>
2025-04-18 01:34:40 +0800

    feat: setup supabase, init tailwind, added ui

---
+++ src/components/Button.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+defineProps<{ msg?: string }>()
+</script>
+
+<template>
+    <button class="border-[1px] border-dashed px-4 py text-2xl cursor-pointer hover:bg-gray-300 transition-all duration-150">{{ msg }}</button>
+</template>
